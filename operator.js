b2a96b23fe46075fa38cb1ba91a03bdbfce68b88
MuhammadHafijur <hafijur.salah@gmail.com>
2021-06-03 20:42:25 +0600

    Getting started with Vanilla JS

---
+++ operator.js
@@ -0,0 +1,48 @@
+                                    // OPERATOR
+
+
+    // 2 + 4 = 6   /*  '+' is a operator and '2' is operand */
+
+/* 
+
+        1. Arithmetic Operator
+
+        2. Comparison Operator
+
+        3. Logical/Relational Operator
+
+        4. Assignment Operator
+
+        5. Conditional/Ternary Operator 
+
+*/
+
+// Arithmetic Operator
+
+var a = 10;
+var b = 20;
+console.log(a + b);
+
+var c = 30;
+var d = 20;
+console.log(c - d);
+
+var e = 10;
+var f = 10;
+console.log(e * f);
+
+var g = 40;
+var h = 10;
+console.log(g / h);
+
+var i = 17;
+var j = 12;
+console.log(i % j);
+
+// Answers
+
+// 30  additiion
+// 10  substraction
+// 100 multipication
+// 4   division
+// 5   modulas (remainder)
---
+++ variable.js
@@ -0,0 +1,26 @@
+var person = 'Hafijur Rahman';
+person = 'Muhammad Hafijur Rahman';
+console.log(person);
+
+var myAge = 24;
+console.log(typeof(myAge));
+
+// Data Type
+
+// Primitive Data Type
+
+/* Number, String, Boolean, Undefined, Null */
+
+
+// Non-Primitive Data Type
+
+/* Array, Object, Function */
+
+
+
+// single line comment
+
+
+/*  Multi 
+            Line
+                    Comment */
